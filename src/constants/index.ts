3197c796d938318486d2fab98d587dae4603ea9f
Phoebe Li <49734328+zpl0310@users.noreply.github.com>
2019-06-20 17:43:00 -0700

    Add files via upload

---
+++ src/constants/index.ts
@@ -0,0 +1,7 @@
+//src/constants/index.ts
+
+export const INCREMENT_COUNT = 'INCREMENT_COUNT';
+export type INCREMENT_COUNT = typeof INCREMENT_COUNT;
+
+export const DECREMENT_COUNT = 'DECREMENT_COUNT';
+export type DECREMENT_COUNT = typeof DECREMENT_COUNT;
